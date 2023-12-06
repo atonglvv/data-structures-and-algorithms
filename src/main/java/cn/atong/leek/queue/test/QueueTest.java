c9dd7f4dc2e6380279b7df0713bf2dc756e966c5
atong <lt1550919167@163.com>
2023-12-06 18:01:01 +0800

    feat: PriorityQueue

--- src/main/java/cn/atong/leek/queue/test/QueueTest.java
+++ src/main/java/cn/atong/leek/queue/test/QueueTest.java
@@ -3,10 +3,10 @@ package cn.atong.leek.queue.test;
 import org.junit.Test;
 
 /**
- * @program: data-structures-and-algorithms
- * @description:
- * @author: atong
- * @create: 2023-12-04 16:30
+ * @program data-structures-and-algorithms
+ * @description
+ * @author atong
+ * @create 2023-12-04 16:30
  */
 public class QueueTest {
 
